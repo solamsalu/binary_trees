414b3c7e980b8b575e749d9bd282475d8e46ecab
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-06-02 05:40:14 +0300

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,208 +1,196 @@
 #include "binary_trees.h"
 
-/**
- * enqueue_item_4 - Adds an item to a queue.
- * @queue_h: A pointer to the queue's head.
- * @queue_t: A pointer to the queue's tail.
- * @n: A pointer to the queue's size value.
- * @item: The item to add to the queue.
- */
-void enqueue_item_4(heap_t **queue_h, heap_t **queue_t,
-	int *n, void *item)
-{
-	heap_t *new_node;
+#define INIT_NODE {0, NULL, NULL, NULL}
 
-	if ((queue_h != NULL) && (queue_t != NULL))
-	{
-		new_node = malloc(sizeof(heap_t));
-		if (new_node == NULL)
-			return;
-		new_node->left = *queue_t;
-		new_node->right = NULL;
-		new_node->n = (item != NULL ? ((heap_t *)item)->n : -1);
-		new_node->parent = (heap_t *)item;
-		if (*queue_h == NULL)
-			*queue_h = new_node;
-		if (*queue_t != NULL)
-			(*queue_t)->right = new_node;
-		*queue_t = new_node;
-		if (n != NULL)
-			(*n)++;
+#define CONVERT "0123456789ABCDEF"
+
+#define SETUP_NODE_BLOC { \
+	tmp = *root; \
+	size = binary_tree_size(*root); \
+	binary = &buffer[49]; \
+	*binary = 0; \
 	}
+
+#define FREE_NODE_BLOC { \
+		res = tmp->n; \
+		free(tmp); \
+		*root = NULL; \
+	}
+
+#define SWAP_HEAD_BLOC { \
+		head = *root; \
+		head = swap_head(head, tmp); \
+		res = head->n; \
+		free(head); \
+		*root = tmp; \
+		tmp = perc_down(tmp); \
+		*root = tmp; \
+	}
+
+#define CONVERT_LOOP { \
+		*--binary = CONVERT[size % 2]; \
+		size /= 2; \
+	}
+
+/**
+ * swap - swaps two nodes in binary tree
+ * @a: first node
+ * @b: second node
+ * Return: pointer to root
+ */
+bst_t *swap(bst_t *a, bst_t *b)
+{
+	bst_t a_copy = INIT_NODE;
+
+	a_copy.n = a->n;
+	a_copy.parent = a->parent;
+	a_copy.left = a->left;
+	a_copy.right = a->right;
+	a->parent = b;
+	a->left = b->left;
+	a->right = b->right;
+	if (b->left)
+		b->left->parent = a;
+	if (b->right)
+		b->right->parent = a;
+
+	b->parent = a_copy.parent;
+	if (a_copy.parent)
+	{
+		if (a == a_copy.parent->left)
+			a_copy.parent->left = b;
+		else
+			a_copy.parent->right = b;
+	}
+	if (b == a_copy.left)
+	{
+		b->left = a;
+		b->right = a_copy.right;
+		if (a_copy.right)
+			a_copy.right->parent = b;
+	}
+	else if (b == a_copy.right)
+	{
+		b->right = a;
+		b->left = a_copy.left;
+		if (a_copy.left)
+			a_copy.left->parent = b;
+	}
+	while (b->parent)
+		b = b->parent;
+	return (b);
 }
 
 /**
- * dequeue_item_4 - Removes an item from a queue.
- * @queue_h: A pointer to the queue's head.
- * @queue_t: A pointer to the queue's tail.
- * @n: A pointer to the queue's size value.
- *
- * Return: The value of the removed queue.
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: input binary tree
+ * Return: number of descendant child nodes
  */
-heap_t *dequeue_item_4(heap_t **queue_h,
-	heap_t **queue_t, int *n)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	heap_t *tmp0;
-	heap_t *tmp1;
-	heap_t *node = NULL;
+	if (!tree)
+		return (0);
 
-	if ((queue_h != NULL) && (queue_t != NULL))
-	{
-		tmp0 = *queue_h;
-		if (tmp0 != NULL)
-		{
-			node = tmp0->parent;
-			if (tmp0->right != NULL)
-			{
-				tmp1 = tmp0->right;
-				tmp1->left = NULL;
-				*queue_h = tmp1;
-				free(tmp0);
-			}
-			else
-			{
-				free(tmp0);
-				*queue_h = NULL;
-				*queue_t = NULL;
-			}
-			if (n != NULL)
-				(*n)--;
-		}
-	}
-	return (node);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
 /**
- * get_last_heap_node - Gets the last level order node \
- * in a max binary heap tree.
- * @root: The root of the max binary heap tree.
- *
- * Return: A pointer to the last level order node, otherwise NULL.
+ * swap_head - helper func to swap head and node
+ * @head: pointer to head
+ * @node: pointer to node
+ * Return: pointer to severed head of tree
  */
-heap_t *get_last_heap_node(const heap_t *root)
+heap_t *swap_head(heap_t *head, heap_t *node)
 {
-	heap_t *head = NULL, *tail = NULL;
-	heap_t *last_node = NULL, *current = NULL;
-	int n = 0;
-
-	if (root != NULL)
+	if (node->parent->left == node)
 	{
-		enqueue_item_4(&head, &tail, &n, (void *)root);
-		while (n > 0)
-		{
-			current = head;
-			if (current->parent != NULL)
-			{
-				last_node = current->parent;
-				if (current->parent->left != NULL)
-				{
-					enqueue_item_4(&head, &tail, &n, (void *)(current->parent->left));
-				}
-				if (current->parent->right != NULL)
-				{
-					enqueue_item_4(&head, &tail, &n, (void *)(current->parent->right));
-				}
-			}
-			dequeue_item_4(&head, &tail, &n);
-		}
-		while (n > 0)
-			dequeue_item_4(&head, &tail, &n);
-	}
-	return (last_node);
+		node->parent->left = NULL;
+	} else
+		node->parent->right = NULL;
+	node->parent = NULL;
+	node->left = head->left;
+	node->right = head->right;
+	if (head->left)
+		head->left->parent = node;
+	if (head->right)
+		head->right->parent = node;
+	return (head);
 }
 
 /**
- * swap_tree_node_with_parent_1 - Swaps a node in a \
- * max binary heap tree with its parent.
- * @node: A pointer to the node's address.
- * @root: A pointer to the root of the tree.
+ * perc_down - percolate head into correct position
+ * @node: pointer to head
+ * Return: pointer to head of tree
  */
-void swap_tree_node_with_parent_1(heap_t **node, heap_t **root)
+heap_t *perc_down(heap_t *node)
 {
-	heap_t *node_copy, *p, *tmp0, *l, *r;
+	int max;
+	heap_t *next = node;
 
-	if ((node != NULL) && ((*node)->parent != NULL) && (root != NULL))
+	if (!node)
+		return (NULL);
+	max = node->n;
+	if (node->left)
+		max = MAX(node->left->n, max);
+	if (node->right)
+		max = MAX(node->right->n, max);
+	if (node->left && max == node->left->n)
+		next = node->left;
+	else if (node->right && max == node->right->n)
+		next = node->right;
+	if (next != node)
 	{
-		p = (*node)->parent, node_copy = *node, tmp0 = (*node)->parent->parent;
-		l = (*node)->left, r = (*node)->right;
-		if ((*node)->parent->left != *node) /* swap from the right */
-		{
-			if ((tmp0 != NULL) && (tmp0->left == (*node)->parent))
-				tmp0->left = *node;
-			if ((tmp0 != NULL) && (tmp0->right == (*node)->parent))
-				tmp0->right = *node;
-			if ((*node)->parent->left != NULL)
-				(*node)->parent->left->parent = node_copy;
-			(*node)->parent = tmp0, (*node)->left = p->left, (*node)->right = p;
-			p->parent = node_copy, p->left = l, p->right = r;
-		}
-		else /* swap from the left */
-		{
-			if ((tmp0 != NULL) && (tmp0->left == p))
-				tmp0->left = *node;
-			if ((tmp0 != NULL) && (tmp0->right == p))
-				tmp0->right = *node;
-			if ((*node)->parent->right != NULL)
-				(*node)->parent->right->parent = *node;
-			(*node)->parent = tmp0, (*node)->right = p->right, (*node)->left = p;
-			p->parent = node_copy, p->left = l, p->right = r;
-		}
-		if (l != NULL)
-			l->parent = p;
-		if (r != NULL)
-			r->parent = p;
-		if (tmp0 == NULL)
-			*root = node_copy;
+		swap(node, next);
+		perc_down(node);
 	}
+	return (next);
 }
 
 /**
  * heap_extract - extracts the root node of a Max Binary Heap
- *		  The root node will be freed and replaced with
- *		  the last level-order node of the heap.
- *		  Once replaced, the heap will be rebuilt if necessary.
- * @root: a double pointer to the root node of heap
- *
- * Return: the value stored in the root node or 0 for a failure
+ * @root: double pointer to root of tree
+ * Return: value stored in the root node
  */
 int heap_extract(heap_t **root)
 {
-	heap_t *node, *node_l, *node_r, *tmp, *dummy;
-	int value = 0, val_l, val_r;
+	size_t size, i;
+	char *binary, c, buffer[50];
+	int res;
+	heap_t *tmp, *head;
 
-	if ((root != NULL) && (*root != NULL))
+	if (!root || !*root)
+		return (0);
+	SETUP_NODE_BLOC;
+	if (size == 1)
 	{
-		node = *root;
-		node_l = node->left;
-		node_r = node->right;
-		value = node->n;
-		tmp = get_last_heap_node(node);
-		*root = ((tmp != NULL) && (tmp->parent != NULL) ? tmp : NULL);
-		if ((tmp != NULL) && (tmp->parent != NULL))
+		FREE_NODE_BLOC;
+		return (res);
+	}
+	do {
+		CONVERT_LOOP;
+	} while (size);
+
+	for (i = 1; i < strlen(binary); i++)
+	{
+		c = binary[i];
+		if (i == strlen(binary) - 1)
 		{
-			if (tmp->parent->left == tmp)
-				tmp->parent->left = NULL;
-			if (tmp->parent->right == tmp)
-				tmp->parent->right = NULL;
-			tmp->parent = NULL;
-			tmp->left = (node_l != tmp ? node_l : NULL);
-			tmp->right = (node_r != tmp ? node_r : NULL);
-			if ((node_l != NULL) && (node_l != tmp))
-				node_l->parent = tmp;
-			if ((node_r != NULL) && (node_r != tmp))
-				node_r->parent = tmp;
-			while (tmp != NULL)
+			if (c == '1')
 			{
-				val_l = tmp->left != NULL ? tmp->left->n : tmp->n;
-				val_r = tmp->right != NULL ? tmp->right->n : tmp->n;
-				dummy = val_l > val_r ? tmp->left : tmp->right;
-				if ((dummy != NULL) && (dummy->n > dummy->parent->n))
-					swap_tree_node_with_parent_1(&dummy, root);
-				else
-					break;
+				tmp = tmp->right;
+				break;
+			}
+			else if (c == '0')
+			{
+				tmp = tmp->left;
+				break;
 			}
 		}
-		free(node);
+		if (c == '1')
+			tmp = tmp->right;
+		else if (c == '0')
+			tmp = tmp->left;
 	}
-	return (value);
+	SWAP_HEAD_BLOC;
+	return (res);
 }
